71e05bcfd5db1f3af3d8f95b32dc1ac2b94f6c14
Neka <nekoanchor@yahoo.fr>
2021-09-28 20:21:40 +0200

    feat: add store

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import TodoScreen from "./src/screens/Tasks"
+import { Provider } from 'react-redux';
+import {store} from '../src/redux/store';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <TodoScreen />
+    <Provider store={store}>
+      <SafeAreaView style={styles.container}>
+        <TodoScreen />
     </SafeAreaView>
+    </Provider>
   );
 }
 
