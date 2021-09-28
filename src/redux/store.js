9aaea7294b4683dcee525192fdbb158e96f43b41
Neka <nekoanchor@yahoo.fr>
2021-09-28 20:20:47 +0200

    feat: add store

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import {createStore} from 'redux';
+
+//Selectors (Pour selectionner une partie du store)
+
+//Actions (fonctions qui renvoient un objet aux reducers : { type: MON_ACTION, payload : { DATA } })
+
+//Reducers (retourne un nouveau state)
+
+//Store (pour stocker et avoir accès partout dans l'app)
+export const store = createStore()
